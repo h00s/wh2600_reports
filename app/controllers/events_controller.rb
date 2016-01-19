5910a8dfdccf55a9a46e318eee4954832b5e8958
Krunoslav Husak <kruno@binel.hr>
2016-01-19 01:46:31 +0100

    Configured default route. Enabled serving static content in production. Ordering events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.order(created_at: :desc).limit(100)
   end
 
   # GET /events/1
