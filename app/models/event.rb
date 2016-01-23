6d85372053a2c1ea183dc574b3c4d31d3e164901
Krunoslav Husak <kruno@binel.hr>
2016-01-23 20:33:57 +0100

    Added wind direction validation.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ActiveRecord::Base
-  validates :pressure_absolute, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
-  validates :pressure_relative, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
   validates :temperature_in, numericality: { greater_than_or_equal_to: -40, less_than_or_equal_to: 60 }
   validates :temperature_out, numericality: { greater_than_or_equal_to: -40, less_than_or_equal_to: 60 }
   validates :humidity_in, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :humidity_out, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :pressure_absolute, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
+  validates :pressure_relative, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
+  validates :wind_direction, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 360 }
 end
