34ca319e5b27d54d631a0f870031cb5fbd38b7b9
Krunoslav Husak <kruno@binel.hr>
2016-01-24 16:58:02 +0100

    Fixed wrong maximum validation value for pressure.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   validates :temperature_out, numericality: { greater_than_or_equal_to: -40, less_than_or_equal_to: 60 }
   validates :humidity_in, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :humidity_out, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :pressure_absolute, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
-  validates :pressure_relative, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000 }
+  validates :pressure_absolute, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1100 }
+  validates :pressure_relative, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1100 }
   validates :wind_direction, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 360 }
 end
